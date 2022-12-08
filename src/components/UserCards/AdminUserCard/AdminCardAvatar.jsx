854d46b2f3de13c4a075c7ccbdaf878d179e54a1
Yuwen <kfk07027@gmail.com>
2022-12-09 06:51:24 +0800

    Feat: add AdminUserAvatar subcomponent

---
+++ src/components/UserCards/AdminUserCard/AdminCardAvatar.jsx
@@ -0,0 +1,7 @@
+const AdminCardAvatar = ({ src, className }) => (
+  <div className={className}>
+    <img src={src} alt="avatar" />
+  </div>
+)
+
+export default AdminCardAvatar
---
+++ src/components/UserCards/AdminUserCard/AdminUserCard.jsx
@@ -0,0 +1,7 @@
+// import AdminCardAvatar from './AdminCardAvatar'
+// import styles from 'assets/styles/components/userCards/adminUserCard.module.scss'
+function AdminUserCard() {
+  return
+}
+
+export default AdminUserCard
