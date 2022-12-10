8431afd2eadb0adf13ad01fa1079ac4e57f68bef
Yuwen <kfk07027@gmail.com>
2022-12-10 16:15:48 +0800

    Fear: add Reply JSX

---
+++ src/components/Reply.jsx
@@ -0,0 +1,22 @@
+import {
+  ContentText,
+  ReplyTargetText,
+  SubText,
+  UserAvatar,
+  UserNameText,
+} from './share'
+import styles from 'assets/styles/components/reply.module.scss'
+
+function Reply({ reply }) {
+  return (
+    <li className={styles.layout}>
+      <UserAvatar src={reply.avatar} />
+      <UserNameText name={reply.name} />
+      <SubText text={`@${reply.account}．${reply.time}`} />
+      <ReplyTargetText name={reply.target} />
+      <ContentText text={reply.content} />
+    </li>
+  )
+}
+
+export default Reply
