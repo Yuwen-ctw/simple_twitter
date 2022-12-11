5b3ff5d1c7ce4cb42b95eeeb0858c763d5387cc2
Yuwen <kfk07027@gmail.com>
2022-12-11 13:38:02 +0800

    Feat: add UserSection JSX

--- src/pages/MainPage/UserSection/UserReplySection.jsx
+++ src/pages/MainPage/UserSection/UserReplySection.jsx
@@ -1,12 +1,19 @@
-import { useParams } from 'react-router-dom'
+// import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Reply from 'components/Reply'
+import db from 'db.json'
 
 function UserRepliesSection() {
-  const { userId } = useParams()
-  return (
-    <>
-      <h1>This is UserReplySection on /main/user/{userId}</h1>
-    </>
-  )
+  // const { userId } = useParams()
+  // TODO get Data and likeList...  same as MainSection
+  const [data, setData] = useState([])
+  useEffect(() => {
+    setData(db.replys)
+  }, [])
+
+  const dataList = data.map((reply) => <Reply key={reply.id} reply={reply} />)
+
+  return <ul>{dataList}</ul>
 }
 
 export default UserRepliesSection
--- src/pages/MainPage/UserSection/UserSectionLayout.jsx
+++ src/pages/MainPage/UserSection/UserSectionLayout.jsx
@@ -2,11 +2,13 @@ import { Outlet, useParams } from 'react-router-dom'
 import { ProfileUserCard } from 'components/UserCards'
 import db from 'db.json'
 import SectionHeader from './SectionHeader'
+import styles from 'assets/styles/pages/userSection.module.scss'
+
 function UserSectionLayout() {
   const { userId } = useParams()
   console.log(`user#${userId}'s personal page`)
   return (
-    <section>
+    <section className={[styles.sectionWrapper, 'scrollbar'].join(' ')}>
       <SectionHeader user={db.loginUser} />
       <ProfileUserCard user={db.loginUser} />
       <Outlet />
